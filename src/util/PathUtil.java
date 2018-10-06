7037482a91ba63c6fa47c9cc9da368d136c66e7a
rbalakrishnan <rbalakrishnan@unomaha.edu>
2018-10-05 20:31:00 -0500

    project

---
+++ src/util/PathUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import project1004_ast_edit_bala.Activator;
+
+
+
+public class PathUtil {
+	public static String getNeighborProjectPath(String projectName) {
+		String thisPluginID = Activator.getContext().getBundle().getSymbolicName();
+		Bundle plugin = Platform.getBundle(thisPluginID);
+		URL url = plugin.getEntry("/");
+		String result = null;
+		try {
+			URL resolvedURL = FileLocator.resolve(url);
+			File file = new File(resolvedURL.getFile());
+			File parentFile = file.getParentFile();
+			File neighborProject = new File(parentFile.getAbsolutePath() + File.separator + projectName);
+			result = neighborProject.getAbsolutePath();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
